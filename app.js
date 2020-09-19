651ad279adb33cb7b3622bf862e7211b546a7f3f
Yusadolat <user@users-MacBook-Air.local>
2020-09-19 20:24:27 +0100

    Addded CorS Origin :zap:

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const halqahRoute = require('./routes/halqahRoute')
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 
 
 
