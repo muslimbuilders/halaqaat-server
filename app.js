c8f68be95b7308723a8cf41cb166634b3735e961
Yusadolat <user@users-MacBook-Air.local>
2020-09-20 18:26:20 +0100

    quick fix for CORS :zap:

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 
 app.use('/api/v1/halqah', halqahRoute)
