1f3a405dc820bb5b870fea6dea587b50784302de
Yusadolat <yusadolat@gmail.com>
2020-12-25 22:05:29 +0100

    Added User Schema :zap:

---
+++ users/userModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    email: { type: String, unique: true},
+    password: { type: String},
+})
+
+
+module.exports = mongoose.model('User', userSchema)
