e32a86fc21bf321a145b778d691ed777a86b124b
Yusadolat <yusadolat@gmail.com>
2021-01-29 23:09:44 +0100

    added jwt..

--- users/userModel.js
+++ users/userModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     email: { type: String, unique: true},
     password: { type: String},
+    confirmPassword: { type: String}
 })
 
 
