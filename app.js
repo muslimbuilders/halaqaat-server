9410a775c35156608cb93de6dba3ffbd51a83514
Yusadolat <yusadolat@gmail.com>
2021-02-06 23:09:23 +0100

    comment manual cors origin

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
 
 app.get('/', (req, res)=> {
   res.send('Hey You, Server is running')
