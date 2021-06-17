f1dcf97b191fd6f3965f29a1d95c7a8a2ae11a45
Yusadolat <yusadolat@gmail.com>
2021-06-17 21:01:30 +0100

    test extended :fire:

--- test/halaqaat.js
+++ test/halaqaat.js
@@ -5,12 +5,14 @@ import { should } from 'chai';
 chai.use(chaiHttp);
 
 describe('*********** Halaqaat ***********', () => {
-  describe('/GET Halqah', () => {
-    it('it should GET all Halaqaat', async function () {
-      const res = await chai.request(app).get('/api/v1/halqah');
+  describe("/POST login", () => {
+    it("it should GET token", async function () {
+      const res = await chai
+        .request(app)
+        .get('/api/v1/halqah')
 
-      res.should(200);
-      res.body.should.be.an('object');
+      res.should.have.status(200);
+      res.body.should.be.an("object");
     });
-  });
 });
+})
