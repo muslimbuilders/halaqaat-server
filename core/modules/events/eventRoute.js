a8bd8041cc9c89d454b367e2f9b023877c817fcb
jocanola <jokanola.it@gmail.com>
2021-12-12 12:04:09 +0100

    Adding userId to each event and halqah, also created an endpoint for getting user basic info using userId from each event or halqah

--- core/modules/events/eventRoute.js
+++ core/modules/events/eventRoute.js
@@ -2,15 +2,24 @@ import route from '../../common/route.js';
 
 import { createEvent, getAllEvents, getEvent } from './eventController.js';
 
-
-const routes = [{
-  path: "/events", handler: getAllEvents, method: 'GET', auth: true
-},
-{
-  path: "/events", handler: createEvent, method: 'POST', auth: true
-},
-{
-  path: "/events/:id", handler: getEvent, method: 'GET', auth: true
-},
-]
+const routes = [
+  {
+    path: '/events',
+    handler: getAllEvents,
+    method: 'GET',
+    auth: true,
+  },
+  {
+    path: '/events/new/:id',
+    handler: createEvent,
+    method: 'POST',
+    auth: false,
+  },
+  {
+    path: '/events/:id',
+    handler: getEvent,
+    method: 'GET',
+    auth: true,
+  },
+];
 export default route(routes);
